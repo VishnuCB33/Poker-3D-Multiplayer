28883aca9c4b50897ee169f58394265dd4374d01
VishnuCB33 <vishnuvegasheri123@gmail.com>
2024-11-08 15:56:38 +0530

    hindi setup

--- Assets/Script/LanguageChangeButton.cs
+++ Assets/Script/LanguageChangeButton.cs
@@ -1,26 +1,55 @@
 using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Localization.Settings;
+
 public class LanguageChangeButton : MonoBehaviour
 {
-    bool isActive=false;
+    public bool isActive = false;
+    public TMP_FontAsset hindFont;     
+    public TMP_FontAsset arvoText;
+    public TextMeshProUGUI languageText; 
+
+
+   /* private void Update()
+    {
+
+        if(Input.GetKeyDown(KeyCode.Escape)) {
+            ChangeFont();
+        }
+    }*/
+
+    public void ChangeFont()
+    {
+        languageText.font = hindFont;
+    }
     public void ChangeLocale(int localeID)
     {
-        if (isActive == true)
+        if (isActive)
         {
             return;
-          
-          
-           
         }
+
+       
+        if (localeID == 2)
+        {
+            languageText.font = hindFont;
+            Debug.Log("Hindi font applied.");
+        }
+        else
+        {
+            languageText.font = arvoText;
+            Debug.Log("Default font applied.");
+        }
+
         StartCoroutine(SetLocale(localeID));
     }
-   IEnumerator SetLocale(int _localeID)
+
+    IEnumerator SetLocale(int _localeID)
     {
         isActive = true;
         yield return LocalizationSettings.InitializationOperation;
-        LocalizationSettings.SelectedLocale=LocalizationSettings.AvailableLocales.Locales[_localeID];
+        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[_localeID];
         isActive = false;
     }
 }
