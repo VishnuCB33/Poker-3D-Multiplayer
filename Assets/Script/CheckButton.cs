6d8716588c336577018b327b032019dd4b6a2809
VishnuCB33 <vishnuvegasheri123@gmail.com>
2024-11-11 17:48:59 +0530

    royalFlush AKQJ10

--- Assets/Script/CheckButton.cs
+++ Assets/Script/CheckButton.cs
@@ -26,37 +26,71 @@ public class CheckButton : MonoBehaviour
     [SerializeField] private List<GameObject> duplicateCheck;
    
 
-  private void Start()
+    private void Start()
     {
        
     }
     private void Update()
     {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            RoyalFlush();
+        }
 
-
-        RoyalFlush();
+      
 
 
     }
     public bool RoyalFlush()
     {
         bool isA = false;
-        int count;
-       List<GameObject>merge=finalCheckFiveCard.Concat(playerOneCard).ToList();
+        bool isK = false;
+        bool isQ = false;
+        bool isJ = false;
+        bool isTen = false;
+        int countA=0;
+        int countK=0;
+        int countQ=0;
+        int countJ=0;
+        int countTen = 0;
+        List<GameObject>merge=finalCheckFiveCard.Concat(playerOneCard).ToList();
        for(int i=0;i<merge.Count;i++)
         {
-           for(int j = 0; j < 5; j++)
-            {
-                switch(merge[i].GetComponent<CardsAttached>().properties.allCards )
+           
+                CardSeries check = merge[i].GetComponent<CardsAttached>().properties.allCards; 
+                switch(check)
                 {
                     case CardSeries.A: isA = true;
+                        countA++;
+                        Debug.Log(countA);
                         break;
-
+                   case CardSeries.K: isK = true;
+                        countK++;
+                        Debug.Log(countK);
+                        break;
+                    case CardSeries.Q: isQ = true;
+                        countQ++;
+                        Debug.Log(countQ);
+                        break;
+                    case CardSeries.J: isJ = true;
+                        countJ++;
+                        Debug.Log(countJ);
+                        break;
+                    case CardSeries.Ten: isTen = true;
+                        countTen++;
+                        Debug.Log(countTen);
+                        break;
+                         
+                       
                 }
-               
+
+
+            if (isA == true && isK == true && isQ == true && isJ == true && isTen == true)
+            {
                
             }
         }
+       
         return isA;
 
     }
