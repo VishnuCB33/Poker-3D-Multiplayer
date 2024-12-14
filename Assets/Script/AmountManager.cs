375519435e8f2a6de2a2f4b7df298809d1382271
smash701 <abinraj7001@gmail.com>
2024-12-14 17:40:22 +0530

    me seat arrange ment vishnu get data
    me seat arrange ment vishnu get data

--- Assets/Script/AmountManager.cs
+++ Assets/Script/AmountManager.cs
@@ -6,13 +6,30 @@ using TMPro;
 using UnityEngine;
 
 public class AmountManager : MonoBehaviour
-{public static AmountManager Instance;
+{ 
+    public static AmountManager Instance;
     [Header("UI")]
     public TextMeshProUGUI amount;
-
+    
     void Start()
     {
         Login();
+       /* string text = amount.text.Trim(); // Get the text and trim any leading/trailing spaces
+
+        int result;
+
+        // Try to parse the string to an integer
+        if (int.TryParse(text, out result))
+        {
+            Debug.Log("Parsed integer: " + result);
+
+            // Display the parsed integer in the TextMeshProUGUI
+            amount.text = result.ToString(); // Convert the integer back to a string and set it in the TextMeshProUGUI
+        }
+        else
+        {
+            Debug.LogError("Failed to parse the value: " + text);
+        }*/
     }
     public void Awake()
     {
